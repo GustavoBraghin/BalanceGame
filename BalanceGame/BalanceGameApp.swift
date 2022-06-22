d95c1a412d055b9ed1769cdb3cdc145c445f3e4f
GustavoBraghin <gugabraghin@gmail.com>
2022-06-22 16:39:53 -0300

    Initial Commit

---
+++ BalanceGame/BalanceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceGameApp.swift
+//  BalanceGame
+//
+//  Created by Gustavo da Silva Braghin on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BalanceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
